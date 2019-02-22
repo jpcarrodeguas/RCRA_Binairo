89dae979cca133d026ff0aacece8c3ab8542c6b9
Julian Penedo Carrodeguas <j.pcarrodeguas@udc.es>
2019-02-22 11:22:14 +0100

    123

--- binario.c
+++ binario.c
@@ -10,7 +10,7 @@ int *init_vector(int dimension) {
 void clasp_solve(int dimension){
 	FILE *fp;
 	char c;
-	char out[1024];
+	char out[4096];
 	char *token;
 	const char s[2] = " ";
 	int i;
@@ -78,31 +78,6 @@ int three_consecutive_rule(FILE *file, int n) {
     return count;
 }
 
-int same_number_of_each_rule(FILE *file, int n, int *vector) {
-    // El array 'vector' contiene la configuración de fichas
-    int count = 0;
-    int i, j, aux;
-
-    for (i = 0; i < n; i++) {
-        // Rule for row
-        for (j = 0; j < n; j++) {
-            aux = (j+1+n*i) * vector[j];
-            fprintf(file, "%d ", aux);
-        }
-        fprintf(file, "0\n");
-        count += 1;
-        // Rule for column
-        for (j = 0; j < n; j++) {
-            aux = (i+j*n+1) * vector[j];
-            fprintf(file, "%d ", aux);
-        }
-        fprintf(file, "0\n");
-        count +=1;
-    }
-
-    return count;
-}
-
 void write_rules(int *vector, int dimension){
 	FILE *file, *tmpfile;
 	int rulenum = 0;
@@ -127,10 +102,6 @@ void write_rules(int *vector, int dimension){
 	}
     
     rulenum += three_consecutive_rule(tmpfile, dimension);
-
-    int vector2[] = {1, 1, -1, 1, 1, -1}; // para testear 'same_number_of_each_rule'
-
-    rulenum += same_number_of_each_rule(tmpfile, dimension, vector2);
 	
 	
 	file = fopen ("binairo.cnf", "w+");
